71c024ce1571a90e6c327427fefc85890853e6c4
Jason <121495300+jason21715@users.noreply.github.com>
2023-04-07 06:26:21 -0700

    Add function skeletons

---
+++ script.js
@@ -0,0 +1,21 @@
+class linkedList {
+  constructor() {}
+
+  append(value) {}
+  prepend(value) {}
+  size() {}
+  head() {}
+  tail() {}
+  at(index) {}
+  pop() {}
+  contains(value) {}
+  find(value) {}
+  toString() {}
+}
+
+class createNode {
+  constructor(value, nextNode) {
+    this.value = null;
+    this.nextNode = null;
+  }
+}
